bb69d8fce51cf2d7081940a5e314566f702072a2
toufiq23 <drrameraaron023@gmail.com>
2020-06-01 20:49:45 +0200

    ready for deployment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Show = require('./show')
+var shows = require('./shows');
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Show shows={shows} />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/show.js
@@ -0,0 +1,65 @@
+var React = require('react');
+
+
+class Title extends React.Component {
+    render() {
+        return (
+            <h3>Title: {this.props.showTitle}</h3>
+        );
+    }
+}
+
+class Poster extends React.Component {
+    render() {
+        return (
+            <img src={this.props.showPoster} alt="Show Poster" style={{height: 400, width: 400}} />
+        );
+    }
+}
+
+class ShowInfo extends React.Component {
+    render(){
+        return (
+            <div>
+                <p>Plot: {this.props.showPlot}</p>
+                <h5>IMDB Rating: {this.props.showRating}</h5>
+            </div>
+        );
+        
+    }
+    
+}
+
+class Show extends React.Component {
+    static defaultProps = {
+        showIndex: 0
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            showIndex: this.props.showIndex
+        };
+    }
+    handleBtnClick() {
+        var totalShows = this.props.shows.length;
+
+        this.setState(function(prevState) {
+            return {
+                showIndex: (prevState.showIndex + 1) % totalShows
+            };
+        });
+    }
+    render() {
+        var show = this.props.shows[this.state.showIndex];
+        return (
+            <div className="text-center">
+                <Title showTitle={show.title} />
+                <Poster showPoster={show.poster} />
+                <ShowInfo showPlot={show.plot} showRating={show.imdbRating} />
+                <button onClick={this.handleBtnClick.bind(this)}>Next Show</button>
+            </div>
+        );
+    }
+};
+
+module.exports = Show;
